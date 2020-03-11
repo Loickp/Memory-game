c7b36d1c134756b6f10a81440a41329e6f7bf101
Loickp <loick.postel@supinfo.com>
2020-03-11 23:45:36 +0100

    test fort flip

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ var max = 10
 var cartes = ['image1', 'image1', 'image2', 'image2', 'image3', 'image3', 'image4', 'image4', 'image5', 'image5', 'image5', 'image5'];
 
 
-for(var i=0; i < 3; i++){
+for(var i=0; i < 2; i++){
     var game = document.getElementById('game');
     var row = document.createElement('div');
 
     row.setAttribute("class", "row");
     game.appendChild(row);
 
-    for(var j=0; j < 6; j++){
+    for(var j=0; j < 3; j++){
         var col = document.createElement('div');
         var img = document.createElement('img');
 
@@ -22,3 +22,12 @@ for(var i=0; i < 3; i++){
         col.appendChild(img);
     }
 }
+
+
+var card = document.querySelectorAll('.card');
+console.log(card);
+card[0].addEventListener('click', flipCard);
+
+function flipCard() {
+    this.classList.toggle('flip');
+  }
