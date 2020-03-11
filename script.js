187fb42b1d471a15e8c13699640ba7519b0a3e75
Loickp <loick.postel@supinfo.com>
2020-03-11 14:00:28 +0100

    New version

--- script.js
+++ script.js
@@ -1,57 +1,24 @@
 var idx_list = []
 var counter = 0
 var max = 10
-var cartes = ['image1', 'image1', 'image2', 'image2', 'image3', 'image3'];
+var cartes = ['image1', 'image1', 'image2', 'image2', 'image3', 'image3', 'image4', 'image4', 'image5', 'image5', 'image5', 'image5'];
 
-function board(){
-    for(var i = 0; i < 6; i++){
-        var td = document.createElement('td');
-        var div = document.createElement('div');
-        var img_back = document.createElement('img');
-        var img_front = document.createElement('img');
-        var idx = Math.floor(Math.random() * 6);
 
-        while (idx_list.includes(idx)) { // Boucle while qui vérifie si il y à un doublon
-            counter++
-            idx = Math.floor(Math.random() * 6);
-            if (counter === max) { // Permet d'éviter une boucle infini
-                break;
-            }
-        }
-        idx_list.push(idx)
+for(var i=0; i < 3; i++){
+    var game = document.getElementById('game');
+    var row = document.createElement('div');
 
-        if (!(i % 3)) {
-            tr = document.createElement('tr');
-            document.getElementById('card_table').appendChild(tr);
-        }
-        td.appendChild(document.createTextNode(cartes[idx]));
+    row.setAttribute("class", "row");
+    game.appendChild(row);
 
-        div.setAttribute("class", "memory-card");
-        div.setAttribute('onclick', 'flipCard()')
+    for(var j=0; j < 6; j++){
+        var col = document.createElement('div');
+        var img = document.createElement('img');
 
-        //img.src = "img-" + idx + ".png";
-        img_back.setAttribute("id", cartes[idx]);
-        img_back.setAttribute("class", "img-back");
-        img_back.src = "card.png";
-        img_back.setAttribute("onclick", "check(this);");
+        col.setAttribute("class", "col");
+        row.appendChild(col);
 
-        img_front.setAttribute("class", "img-front");
-        img_front.src = "card-front.png";
-
-        td.appendChild(div);
-        div.appendChild(img_back);
-        div.appendChild(img_front);
-        tr.appendChild(td);
+        img.src = "card.png"
+        col.appendChild(img);
     }
 }
-
-function check(card){
-    console.log(card.id);
-}
-
-function flipCard(){
-    //this.classList.toggle('flip');
-    console.log("flip");
-}
-
-board();
