a2a368b4c1f7f4c5a5e4c183485d25c812a9d9f5
Loickp <loick.postel@supinfo.com>
2020-03-16 13:49:36 +0100

    Adding shuffle card

--- script.js
+++ script.js
@@ -1,7 +1,56 @@
 var cardOpen = []
 var cardMatch = []
 
+var cards_list = ['card-1', 'card-1', 'card-2', 'card-2', 'card-3', 'card-3']
+var idx_list = []
+var max = 6
+var counter = 0
+
+function shuffle(array){
+    var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+}
+
+function board(){
+    var table = document.getElementById('game')
+    var card_shuffle = shuffle(cards_list);
+
+    for(var i = 0; i < 6; i++){
+        var td = document.createElement('td')
+        var img = document.createElement('img')
+
+        if(!(i % 3)){
+            var tr = document.createElement('tr')
+            table.appendChild(tr)
+        }
+
+        img.src = "card.png"
+        img.setAttribute('class', 'card-class')
+        img.setAttribute('id', card_shuffle[i])
+
+        tr.appendChild(td)
+        td.appendChild(img)  
+    }
+}
+
+board()
+
 var cards = document.querySelectorAll('.card-class');
+console.log(cards)
 
 cards.forEach(cards => {
     cards.addEventListener('click', function(){
@@ -59,5 +108,9 @@ function disable(){
 function checkEnd(){
     if(cardMatch.length == 6){
         console.log("You win !")
+
+        /*cards.forEach(cards => {
+            cards.src = "card.png"
+        })*/
     }
 }
